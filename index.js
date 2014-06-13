c445f65bf3b48390e4484a1bb79c09316fe1d967
mattdesl <dave.des@gmail.com>
2014-06-13 16:55:43 -0400

    using klasse for cleaner mixin and constructor

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ var wrapContext = require('kami-util').wrapContext;
 
 var colorToFloat = require('number-util').colorToFloat;
 
+var BaseMixins = require('./mixins');
+
 /** 
  * A batcher, typically used for quads (2 tris, indexed), but may
  * also be used for other dynamic types of geometry. This is generally
@@ -41,255 +43,23 @@ var colorToFloat = require('number-util').colorToFloat;
  */
 var BaseBatch = new Class({
 
+	Mixins: BaseMixins,
+
 	//Constructor
 	initialize: function BaseBatch(context, options) {
 		if (!context || typeof context !== "object")
 			throw "valid GL context not specified to SpriteBatch";
 		this.context = wrapContext(context);
 		options = options||{};
-		var size = options.size;
 
-		this.size = size || 500;
+		var shader = this._createShader();
 		
-		// 65535 is max index, so 65535 / 6 = 10922.
-		if (this.size > 10922)
-			throw "Can't have more than 10922 sprites per batch: " + this.size;
-		
-		this._blendSrc = this.context.gl.ONE;
-		this._blendDst = this.context.gl.ONE_MINUS_SRC_ALPHA
-		this._blendingEnabled = true;
-		this._shader = this._createShader();
-
-		/**
-		 * This shader will be used whenever "null" is passed
-		 * as the batch's shader. 
-		 *
-		 * @property {ShaderProgram} shader
-		 */
-		this.defaultShader = this._shader;
-
-		/**
-		 * By default, a SpriteBatch is created with its own ShaderProgram,
-		 * stored in `defaultShader`. If this flag is true, on deleting the SpriteBatch, its
-		 * `defaultShader` will also be deleted. If this flag is false, no shaders
-		 * will be deleted on destroy.
-		 *
-		 * Note that if you re-assign `defaultShader`, you will need to dispose the previous
-		 * default shader yoursel. 
-		 *
-		 * @property ownsShader
-		 * @type {Boolean}
-		 */
-		this.ownsShader = true;
-
 		this.idx = 0;
 
-		/**
-		 * Whether we are currently drawing to the batch. Do not modify.
-		 * 
-		 * @property {Boolean} drawing
-		 */
-		this.drawing = false;
+		this.mesh = this._createMesh(options.size);
 
-		this.mesh = this._createMesh(this.size);
-
-		/**
-		 * The ABGR packed color, as a single float. The default
-		 * value is the color white (255, 255, 255, 255).
-		 *
-		 * @property {Number} color
-		 * @readOnly 
-		 */
-		this.color = colorToFloat(255, 255, 255, 255);
-		
-		/**
-		 * Whether to premultiply alpha on calls to setColor. 
-		 * This is true by default, so that we can conveniently write:
-		 *
-		 *     batch.setColor(1, 0, 0, 0.25); //tints red with 25% opacity
-		 *
-		 * If false, you must premultiply the colors yourself to achieve
-		 * the same tint, like so:
-		 *
-		 *     batch.setColor(0.25, 0, 0, 0.25);
-		 * 
-		 * @property premultiplied
-		 * @type {Boolean}
-		 * @default  true
-		 */
-		this.premultiplied = true;
-	},
-
-	/**
-	 * A property to enable or disable blending for this sprite batch. If
-	 * we are currently drawing, this will first flush the batch, and then
-	 * update GL_BLEND state (enabled or disabled) with our new value.
-	 * 
-	 * @property {Boolean} blendingEnabled
-	 */
-	blendingEnabled: {
-		set: function(val) {
-			var old = this._blendingEnabled;
-			if (this.drawing)
-				this.flush();
-
-			this._blendingEnabled = val;
-
-			//if we have a new value, update it.
-			//this is because blend is done in begin() / end() 
-			if (this.drawing && old != val) {
-				var gl = this.context.gl;
-				if (val)
-					gl.enable(gl.BLEND);
-				else
-					gl.disable(gl.BLEND);
-			}
-
-		},
-
-		get: function() {
-			return this._blendingEnabled;
-		}
-	},
-
-	/**
-	 * Sets the blend source parameters. 
-	 * If we are currently drawing, this will flush the batch.
-	 *
-	 * Setting either src or dst to `null` or a falsy value tells the SpriteBatch
-	 * to ignore gl.blendFunc. This is useful if you wish to use your
-	 * own blendFunc or blendFuncSeparate. 
-	 * 
-	 * @property {GLenum} blendDst 
-	 */
-	blendSrc: {
-		set: function(val) {
-			if (this.drawing)
-				this.flush();
-			this._blendSrc = val;
-		},
-
-		get: function() {
-			return this._blendSrc;
-		}
-	},
-
-	/**
-	 * Sets the blend destination parameters. 
-	 * If we are currently drawing, this will flush the batch.
-	 *
-	 * Setting either src or dst to `null` or a falsy value tells the SpriteBatch
-	 * to ignore gl.blendFunc. This is useful if you wish to use your
-	 * own blendFunc or blendFuncSeparate. 
-	 *
-	 * @property {GLenum} blendSrc 
-	 */
-	blendDst: {
-		set: function(val) {
-			if (this.drawing)
-				this.flush();
-			this._blendDst = val;
-		},
-
-		get: function() {
-			return this._blendDst;
-		}
-	},
-
-	/**
-	 * Sets the blend source and destination parameters. This is 
-	 * a convenience function for the blendSrc and blendDst setters.
-	 * If we are currently drawing, this will flush the batch.
-	 *
-	 * Setting either to `null` or a falsy value tells the SpriteBatch
-	 * to ignore gl.blendFunc. This is useful if you wish to use your
-	 * own blendFunc or blendFuncSeparate. 
-	 *
-	 * @method  setBlendFunction
-	 * @param {GLenum} blendSrc the source blend parameter
-	 * @param {GLenum} blendDst the destination blend parameter
-	 */
-	setBlendFunction: function(blendSrc, blendDst) {
-		this.blendSrc = blendSrc;
-		this.blendDst = blendDst;
-	},
-
-	/**
-	 * This is a setter/getter for this batch's current ShaderProgram.
-	 * If this is set when the batch is drawing, the state will be flushed
-	 * to the GPU and the new shader will then be bound.
-	 *
-	 * If `null` or a falsy value is specified, the batch's `defaultShader` will be used. 
-	 *
-	 * Note that shaders are bound on batch.begin().
-	 *
-	 * @property shader
-	 * @type {ShaderProgram}
-	 */
-	shader: {
-		set: function(val) {
-			var wasDrawing = this.drawing;
-
-			if (wasDrawing) {
-				this.end(); //unbinds the shader from the mesh
-			}
-
-			this._shader = val ? val : this.defaultShader;
-
-			if (wasDrawing) {
-				this.begin();
-			}
-		},
-
-		get: function() {
-			return this._shader;
-		}
-	},
-
-	/**
-	 * Sets the color of this sprite batcher, which is used in subsequent draw
-	 * calls. This does not flush the batch.
-	 *
-	 * If r, g, b, are all numbers, this method assumes that RGB 
-	 * or RGBA float values (0.0 to 1.0) are being passed. Alpha defaults to one
-	 * if undefined.
-	 * 
-	 * If one or more of the (r, g, b) arguments are non-numbers, we only consider the first argument
-	 * and assign it to all four components -- this is useful for setting transparency 
-	 * in a premultiplied alpha stage.
-	 * 
-	 * If the first argument is invalid or not a number,
-	 * the color defaults to (1, 1, 1, 1).
-	 *
-	 * @method  setColor
-	 * @param {Number} r the red component, normalized
-	 * @param {Number} g the green component, normalized
-	 * @param {Number} b the blue component, normalized
-	 * @param {Number} a the alpha component, normalized
-	 */
-	setColor: function(r, g, b, a) {
-		var rnum = typeof r === "number";
-		if (rnum
-				&& typeof g === "number"
-				&& typeof b === "number") {
-			//default alpha to one 
-			a = typeof a === "number" ? a : 1.0;
-		} else {
-			r = g = b = a = rnum ? r : 1.0;
-		}
-		
-		if (this.premultiplied) {
-			r *= a;
-			g *= a;
-			b *= a;
-		}
-		
-		this.color = colorToFloat(
-			~~(r * 255),
-			~~(g * 255),
-			~~(b * 255),
-			~~(a * 255)
-		);
+		//set up the properties for our base batch...
+		BaseMixins.call(this, this.context.gl, shader);
 	},
 
 	/**
@@ -304,6 +74,11 @@ var BaseBatch = new Class({
 	 * @param {Number} size the size passed through the constructor
 	 */
 	_createMesh: function(size) {
+		size = size||500;
+		// 65535 is max index, so 65535 / 6 = 10922.
+		if (size > 10922)
+			throw "Can't have more than 10922 sprites per batch: " + size;
+		
 		//the total number of floats in our batch
 		var numVerts = size * 4 * this.getVertexSize();
 		//the total number of indices in our batch
@@ -478,7 +253,7 @@ var BaseBatch = new Class({
 	destroy: function() {
 		this.vertices = null;
 		this.indices = null;
-		this.size = this.maxVertices = 0;
+		this.maxVertices = 0;
 
 		if (this.ownsShader && this.defaultShader)
 			this.defaultShader.destroy();
---
+++ mixins.js
@@ -0,0 +1,240 @@
+var colorToFloat = require('number-util').colorToFloat;
+var Class = require('klasse');
+
+module.exports = new Class({
+
+	initialize: 
+	function BaseBatchMixins(gl, shader) {
+		this._blendSrc = gl.ONE;
+		this._blendDst = gl.ONE_MINUS_SRC_ALPHA;
+		this._blendingEnabled = true;
+		this._shader = shader;
+
+		/**
+		 * This shader will be used whenever "null" is passed
+		 * as the batch's shader. 
+		 *
+		 * @property {ShaderProgram} shader
+		 */
+		this.defaultShader = this._shader;
+
+		/**
+		 * By default, a SpriteBatch is created with its own ShaderProgram,
+		 * stored in `defaultShader`. If this flag is true, on deleting the SpriteBatch, its
+		 * `defaultShader` will also be deleted. If this flag is false, no shaders
+		 * will be deleted on destroy.
+		 *
+		 * Note that if you re-assign `defaultShader`, you will need to dispose the previous
+		 * default shader yoursel. 
+		 *
+		 * @property ownsShader
+		 * @type {Boolean}
+		 */
+		this.ownsShader = true;
+
+		/**
+		 * Whether we are currently drawing to the batch. Do not modify.
+		 * 
+		 * @property {Boolean} drawing
+		 */
+		this.drawing = false;
+
+		/**
+		 * The ABGR packed color, as a single float. The default
+		 * value is the color white (255, 255, 255, 255).
+		 *
+		 * @property {Number} color
+		 * @readOnly 
+		 */
+		this.color = colorToFloat(255, 255, 255, 255);
+		
+		/**
+		 * Whether to premultiply alpha on calls to setColor. 
+		 * This is true by default, so that we can conveniently write:
+		 *
+		 *     batch.setColor(1, 0, 0, 0.25); //tints red with 25% opacity
+		 *
+		 * If false, you must premultiply the colors yourself to achieve
+		 * the same tint, like so:
+		 *
+		 *     batch.setColor(0.25, 0, 0, 0.25);
+		 * 
+		 * @property premultiplied
+		 * @type {Boolean}
+		 * @default  true
+		 */
+		this.premultiplied = true;
+	},
+
+	/**
+	 * A property to enable or disable blending for this sprite batch. If
+	 * we are currently drawing, this will first flush the batch, and then
+	 * update GL_BLEND state (enabled or disabled) with our new value.
+	 * 
+	 * @property {Boolean} blendingEnabled
+	 */
+	blendingEnabled: {
+		set: function(val) {
+			var old = this._blendingEnabled;
+			if (this.drawing)
+				this.flush();
+
+			this._blendingEnabled = val;
+
+			//if we have a new value, update it.
+			//this is because blend is done in begin() / end() 
+			if (this.drawing && old != val) {
+				var gl = this.context.gl;
+				if (val)
+					gl.enable(gl.BLEND);
+				else
+					gl.disable(gl.BLEND);
+			}
+
+		},
+
+		get: function() {
+			return this._blendingEnabled;
+		}
+	},
+
+	/**
+	 * Sets the blend source parameters. 
+	 * If we are currently drawing, this will flush the batch.
+	 *
+	 * Setting either src or dst to `null` or a falsy value tells the SpriteBatch
+	 * to ignore gl.blendFunc. This is useful if you wish to use your
+	 * own blendFunc or blendFuncSeparate. 
+	 * 
+	 * @property {GLenum} blendDst 
+	 */
+	blendSrc: {
+		set: function(val) {
+			if (this.drawing)
+				this.flush();
+			this._blendSrc = val;
+		},
+
+		get: function() {
+			return this._blendSrc;
+		}
+	},
+
+	/**
+	 * Sets the blend destination parameters. 
+	 * If we are currently drawing, this will flush the batch.
+	 *
+	 * Setting either src or dst to `null` or a falsy value tells the SpriteBatch
+	 * to ignore gl.blendFunc. This is useful if you wish to use your
+	 * own blendFunc or blendFuncSeparate. 
+	 *
+	 * @property {GLenum} blendSrc 
+	 */
+	blendDst: {
+		set: function(val) {
+			if (this.drawing)
+				this.flush();
+			this._blendDst = val;
+		},
+
+		get: function() {
+			return this._blendDst;
+		}
+	},
+
+	/**
+	 * Sets the blend source and destination parameters. This is 
+	 * a convenience function for the blendSrc and blendDst setters.
+	 * If we are currently drawing, this will flush the batch.
+	 *
+	 * Setting either to `null` or a falsy value tells the SpriteBatch
+	 * to ignore gl.blendFunc. This is useful if you wish to use your
+	 * own blendFunc or blendFuncSeparate. 
+	 *
+	 * @method  setBlendFunction
+	 * @param {GLenum} blendSrc the source blend parameter
+	 * @param {GLenum} blendDst the destination blend parameter
+	 */
+	setBlendFunction: function(blendSrc, blendDst) {
+		this.blendSrc = blendSrc;
+		this.blendDst = blendDst;
+	},
+
+	/**
+	 * This is a setter/getter for this batch's current ShaderProgram.
+	 * If this is set when the batch is drawing, the state will be flushed
+	 * to the GPU and the new shader will then be bound.
+	 *
+	 * If `null` or a falsy value is specified, the batch's `defaultShader` will be used. 
+	 *
+	 * Note that shaders are bound on batch.begin().
+	 *
+	 * @property shader
+	 * @type {ShaderProgram}
+	 */
+	shader: {
+		set: function(val) {
+			var wasDrawing = this.drawing;
+
+			if (wasDrawing) {
+				this.end(); //unbinds the shader from the mesh
+			}
+
+			this._shader = val ? val : this.defaultShader;
+
+			if (wasDrawing) {
+				this.begin();
+			}
+		},
+
+		get: function() {
+			return this._shader;
+		}
+	},
+
+	/**
+	 * Sets the color of this sprite batcher, which is used in subsequent draw
+	 * calls. This does not flush the batch.
+	 *
+	 * If r, g, b, are all numbers, this method assumes that RGB 
+	 * or RGBA float values (0.0 to 1.0) are being passed. Alpha defaults to one
+	 * if undefined.
+	 * 
+	 * If one or more of the (r, g, b) arguments are non-numbers, we only consider the first argument
+	 * and assign it to all four components -- this is useful for setting transparency 
+	 * in a premultiplied alpha stage.
+	 * 
+	 * If the first argument is invalid or not a number,
+	 * the color defaults to (1, 1, 1, 1).
+	 *
+	 * @method  setColor
+	 * @param {Number} r the red component, normalized
+	 * @param {Number} g the green component, normalized
+	 * @param {Number} b the blue component, normalized
+	 * @param {Number} a the alpha component, normalized
+	 */
+	setColor: function(r, g, b, a) {
+		var rnum = typeof r === "number";
+		if (rnum
+				&& typeof g === "number"
+				&& typeof b === "number") {
+			//default alpha to one 
+			a = typeof a === "number" ? a : 1.0;
+		} else {
+			r = g = b = a = rnum ? r : 1.0;
+		}
+		
+		if (this.premultiplied) {
+			r *= a;
+			g *= a;
+			b *= a;
+		}
+		
+		this.color = colorToFloat(
+			~~(r * 255),
+			~~(g * 255),
+			~~(b * 255),
+			~~(a * 255)
+		);
+	},
+});
